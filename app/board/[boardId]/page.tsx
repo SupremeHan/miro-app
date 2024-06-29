31574e2727136dd8d5070b86c6ed0a8591968da6
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-06-29 17:04:58 +0200

    Board canvas layout

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Canvas } from "./_components/canvas";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return <Canvas boardId={params.boardId} />;
+};
+
+export default BoardIdPage;
