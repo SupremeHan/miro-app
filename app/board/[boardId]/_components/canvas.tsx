96b3984a95b1c1a5184acf3376df52d07e56c1d9
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-06-29 22:55:25 +0200

    Added logic for info component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useSelf } from "@liveblocks/react/suspense";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -10,12 +9,9 @@ interface CanvasProps {
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const { id, info } = useSelf();
-
-  console.log(id, info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
